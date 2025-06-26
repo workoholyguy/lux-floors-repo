7ec66da68ccf3bcf35a5b02c92a93b587d84dbb0
Omar Madjitov <95881196+workoholyguy@users.noreply.github.com>
2025-06-26 12:08:52 -0400

    Optimized the Navbar

--- src/app/inspiration/page.tsx
+++ src/app/inspiration/page.tsx
@@ -115,7 +115,7 @@ export default function Inspiration() {
           <p className="px-8">Ready to bring your vision to life? Contact us to discuss your project or schedule a visit to our showroom for a personalized consultation.
           </p>
         </section>
-          <ContactUs className='px-8 mb-2'/>
+          <ContactUs />
       </main>
     </div>
   );
